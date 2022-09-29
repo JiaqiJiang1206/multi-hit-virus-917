4458843407d737edcd576ce32e50a834b64baad8
jjq <jiangjiaqi1206@gmail.com>
2022-09-29 16:31:49 +0800

    sdf

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+const cors = require('cors');
+
+app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 var PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
